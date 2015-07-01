0c18b67bff6085a28e63a9a4ebba3dc3a79810a9
jeff dambly <dambly@gmail.com>
2015-06-30 20:20:55 -0700

    created netscaler class to deal with expect, and added VipManager

---
+++ vips/fetch.py
@@ -0,0 +1,82 @@
+import re
+from pexpect import spawn, TIMEOUT
+
+SSH_NEWKEY = 'Are you sure you want to continue connecting'
+
+class Netscaler(object):
+    """
+    wrapper class for interacting with expect and the netscaler
+    """
+    def __init__(self,
+                 host='',
+                 username='',
+                 password=''
+                 ):
+        """
+        :param host: string
+        :param username: string
+        :param password: string
+        """
+        self.host = host
+        self.username = username
+        self.password = password
+        self.prompt = '>'
+
+    def _send(self, command=''):
+
+        self._session()
+        self.session.sendline(command)
+        self.session.expect(self.prompt)
+        result = self.session.before
+        self.session.sendline('exit')
+        return result
+
+    def _session(self):
+        """
+        attempt to create an expect session, if there is timeout return false
+        :return: true or false
+        """
+        session = spawn('ssh %s@%s' % (self.username, self.host))
+        session.delaybeforesend = 1
+        session.timeout = 4
+        retval = session.expect([SSH_NEWKEY, 'password:'])
+
+        if retval == 0:
+            session.sendline('yes')
+            session.expect('password:')
+
+        session.sendline(self.password)
+
+        session.expect(self.prompt)
+        self.session = session
+        return True
+
+    def get_vips(self):
+        """
+        returns the output form 'show server' this is a list of vips
+        :return:
+        """
+
+        data = self._send('show server')
+        # parse out the data so we get a format that can be chanted into a dict
+        data = re.sub(r'\r\n\d+\)|\r\n', '', data)
+        data = re.sub(r':\s+', ':', data)
+        data = re.split(r'\s+', data)[3:]
+        result = {}
+        id = 0
+
+        for i in xrange(0, len(data), 3):
+            temp = {}
+
+            for j in range(0, 3):
+                if data[i+j] == 'Done':
+                    break
+
+                temp2 = re.split(r':', data[i+j])
+                temp.update({temp2[0]: temp2[1]})
+
+            if temp != {}:
+                result[id] = temp
+                id += 1
+
+        return result
--- vips/models.py
+++ vips/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from vips.fetch import Netscaler
+
 
 class Login(models.Model):
     user = models.CharField(max_length=16)
@@ -18,11 +20,37 @@ class Device(models.Model):
     def __unicode__(self):
         return '%s' % self.label
 
+class VipManager(models.Manager):
+
+    def poll(self, device=Device):
+        """
+        takes a query object of type Device, connects to that host, and then gets
+        all the vip info
+
+        then all the information is entered into the database
+        :param device: query object
+        :return:
+        """
+
+        remote = Netscaler(host=device.ip,
+                           username=device.login.user,
+                           password=device.login.password)
+
+        for vipkey, vipval in remote.get_vips().iteritems():
+            Vip.objects.update_or_create(name=vipval['Name'],
+                                         address=vipval['IPAddress'],
+                                         state=vipval['State'],
+                                         device=device)
+
+
 class Vip(models.Model):
     name = models.CharField(max_length=64)
     state = models.CharField(max_length=16)
     address = models.GenericIPAddressField()
     device = models.ForeignKey(Device)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = VipManager()
 
     def __unicode__(self):
         return '%s' % self.name
