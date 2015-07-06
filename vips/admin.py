cd03fb3f45fe61e57656f2eff92cbd5305c710c2
jeff dambly <dambly@gmail.com>
2015-07-06 16:25:50 -0700

    added device

--- vips/admin.py
+++ vips/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from vips.models import Login, Vip, Member
+from vips.models import Login, Vip, Member, Device
 
 admin.site.register(Login)
 admin.site.register(Vip)
 admin.site.register(Member)
+admin.site.register(Device)
