2c698f6f6b0b4c85058b36f061c7f61667baab7f
jeff dambly <dambly@gmail.com>
2015-07-14 15:37:43 -0700

    added login_required

--- netscaler/urls.py
+++ netscaler/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls', namespace='account')),
     url(r'^', include('vips.urls', namespace='vips' )),
-]
+)
